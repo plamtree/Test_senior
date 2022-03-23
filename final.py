67338d9f7c4f2dafcc8c0cdf1524cc65f8b6dba3
plamtree <77606272+plamtree@users.noreply.github.com>
2022-03-23 22:23:15 +0700

    Update final.py

--- final.py
+++ final.py
@@ -182,7 +182,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=10,
+                epochs=200,
                 layers='heads')
 
 
